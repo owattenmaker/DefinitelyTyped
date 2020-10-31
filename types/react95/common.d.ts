3991ac12e2dd1d0854295394aaaf03011ac0f684
Owen Wattenmaker <owen.wattenmaker@colorado.edu>
2020-10-31 01:20:57 -0700

    React95 component library types!

---
+++ types/react95/common.d.ts
@@ -0,0 +1,2 @@
+export const styleReset: string;
+export function createScrollbars(variant?: 'default' | 'flat'): string;
---
+++ types/react95/react95-tests.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as React95 from 'react95';
+
+const d = <React95.Avatar square={false} />;
+const c = <React95.ColorInput variant="default" />;
+const cb = <React95.CheckBox label="default" />;
+const tt = <React95.Tooltip text="hello">world</React95.Tooltip>;
+const tabWithTabs = <React95.Tabs><React95.Tab>hello</React95.Tab></React95.Tabs>;
+const fieldset = <React95.Fieldset label={<div>my label</div>}>this is a fieldset!</React95.Fieldset>;
